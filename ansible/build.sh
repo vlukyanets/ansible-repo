2daa57f184d259c4c188d206e7de8e247f2c82f4
Valentin Lukyanets <valikluks95@protonmail.com>
2022-02-12 20:42:40 +0200

    Added entrypoint to handle calling different scripts of Ansible
    Signed-off-by: Valentin Lukyanets <valikluks95@protonmail.com>

--- ansible/build.sh
+++ ansible/build.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 if [[ -z $1 ]] ; then
-	docker build . -t ansible:latest
+	docker build . -t darkness1prophet/ansible:latest
 else
-	docker build . -t ansible:v$1 --build-arg ANSIBLE_VERSION=$1
+	docker build . -t darkness1prophet/ansible:v$1 --build-arg ANSIBLE_VERSION=$1
 fi
---
+++ ansible/content/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TOOL=$1
+if [ -z "$1" ] ;
+then
+    $HOME/.local/bin/ansible --help ;
+elif [ -f "$HOME/.local/bin/$TOOL" ] ;
+then
+    $HOME/.local/bin/$TOOL ${@: 2} ;
+elif [ -f "$HOME/.local/bin/ansible-$TOOL" ] ;
+then
+    $HOME/.local/bin/ansible-$TOOL ${@: 2} ;
+else
+    echo "No ansible tool '$TOOL'"
+fi
