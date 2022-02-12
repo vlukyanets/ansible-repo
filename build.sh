55f83f859daf408f76aa5085ceabac23ef944daf
Valentin Lukyanets <valikluks95@protonmail.com>
2022-02-12 21:50:09 +0200

    Fixed indents and possible space warnings
    Signed-off-by: Valentin Lukyanets <valikluks95@protonmail.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -z $1 ]] ; then
-	docker build . -t darkness1prophet/ansible:latest
+  docker build . -t darkness1prophet/ansible:latest
 else
-	docker build . -t darkness1prophet/ansible:v$1 --build-arg ANSIBLE_VERSION=$1
+  docker build . -t darkness1prophet/ansible:v$1 --build-arg ANSIBLE_VERSION=$1
 fi
