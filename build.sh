1b70f83fd8c9ec9d80d8f225ef75229803c553bc
Valentin Lukyanets <valikluks95@protonmail.com>
2022-02-16 16:33:10 +0200

    Removed all unnecessary. Saved just playbook
    Signed-off-by: Valentin Lukyanets <valikluks95@protonmail.com>

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ -z $1 ]]
-then
-    docker build ./docker-build -t darkness1prophet/ansible:latest
-else
-    docker build ./docker-build -t darkness1prophet/ansible:v$1 --build-arg ANSIBLE_VERSION=$1
-fi
